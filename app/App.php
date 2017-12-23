0c6a09676ad1cbdd02d39f4a67d2923866920258
f4bian <faaabs@googlemail.com>
2017-12-23 21:32:08 +0100

    Container now implements ArrayAccess Interface. Not fully implemented right now

--- app/App.php
+++ app/App.php
@@ -2,8 +2,27 @@
 
 namespace App;
 
-
+/**
+ * Class App
+ * Main class of the framework. Needs to be created first.
+ * @package App
+ */
 class App
 {
+    protected $container;
+    
+    public function __construct()
+    {
+        $this->container = new Container();
+    }
+
+    /**
+     * @return Container
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
 
 }
--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
+/**
+ * This file is used as bootstrap and index file
+ */
 require_once __DIR__ . "/vendor/autoload.php";
 
 use App\App;
 
 $app = new App;
-var_dump($app);
+
+$container = $app->getContainer();
+
+$container['config'] = function () {
+    return [
+        'db_driver' => 'mysql',
+        'db_host' => 'localhost',
+        'db_name' => 'miniframework',
+        'db_user' => 'homestead',
+        'db_password' => 'secret',
+    ];
+};
+
+var_dump($container->has('config'));
